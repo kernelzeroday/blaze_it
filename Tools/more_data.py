5bf8e3c5d9480ea69bb1725a43f9e459513a692b
kelsey <enktal@gmail.com>
2022-12-02 17:35:20 -0800

    volcanos, avalanche parse

--- Tools/more_data.py
+++ Tools/more_data.py
@@ -67,3 +67,74 @@ earthquakes = pd.read_csv(url, skiprows=1, header=None)
 #save the data to a csv file
 earthquakes.to_csv('earthquakes.csv')
 
+
+#download csv from national wildfire database
+url = 'https://rmgsc.cr.usgs.gov/outgoing/GeoMAC/current_year_fire_data/fire_archive_M6_96619.csv'
+response = requests.get(url)
+#put it into the first frame of a pandas dataframe
+wildfires = pd.read_csv(url, skiprows=1, header=None)
+#save the data to a csv file
+wildfires.to_csv('wildfires.csv')
+
+#download csv from national flood database
+url = 'https://water.weather.gov/ahps2/hydrograph_to_xml.php?gage=mlrf2&output=csv'
+response = requests.get(url)
+#put it into the first frame of a pandas dataframe
+floods = pd.read_csv(url, skiprows=1, header=None)
+#save the data to a csv file
+floods.to_csv('floods.csv')
+
+#download csv from national drought database
+url = 'https://www.ncdc.noaa.gov/drought/monitor/monitor.csv'
+response = requests.get(url)
+#put it into the first frame of a pandas dataframe
+droughts = pd.read_csv(url, skiprows=1, header=None)
+#save the data to a csv file
+droughts.to_csv('droughts.csv')
+
+#download csv from national landslide database
+url = 'https://www.ngdc.noaa.gov/hazard/landslide_data.shtml'
+response = requests.get(url)
+#parse the html
+soup = BeautifulSoup(response.text, 'html.parser')
+#find the table
+table = soup.find('table')
+#find the rows
+rows = table.find_all('tr')
+#find the columns
+columns = [v.text.replace('\n', '') for v in rows[0].find_all('th')]
+#put the data into a pandas dataframe
+landslides = pd.DataFrame(columns=columns)
+for i in range(1, len(rows)):
+    tds = rows[i].find_all('td')
+    if len(tds) == 5:
+        values = [tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text.replace('\n', '')]
+    else:
+        values = [td.text.replace('\n', '') for td in tds]
+    landslides = landslides.append(pd.Series(values, index=columns), ignore_index=True)
+#save the data to a csv file
+landslides.to_csv('landslides.csv')
+
+#download csv from national volcanic eruption database
+url = 'https://www.ngdc.noaa.gov/hazard/volcano_data.shtml'
+response = requests.get(url)
+#parse the html
+soup = BeautifulSoup(response.text, 'html.parser')
+#find the table
+table = soup.find('table')
+#find the rows  
+rows = table.find_all('tr')
+#find the columns
+columns = [v.text.replace('\n', '') for v in rows[0].find_all('th')]
+#put the data into a pandas dataframe
+volcanoes = pd.DataFrame(columns=columns)
+for i in range(1, len(rows)):
+    tds = rows[i].find_all('td')
+    if len(tds) == 5:
+        values = [tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text.replace('\n', '')]
+    else:
+        values = [td.text.replace('\n', '') for td in tds]
+    volcanoes = volcanoes.append(pd.Series(values, index=columns), ignore_index=True)
+#save the data to a csv file
+volcanoes.to_csv('volcanoes.csv')
+
